c207b831372f906f3b80f86aaabe4b99c67f15cf
sohan004 <aryansohan02@gmail.com>
2024-08-13 19:40:00 +0600

    Changes committed on 2024-08-13 19:40:00

--- middleware/rootMiddleware.js
+++ middleware/rootMiddleware.js
@@ -6,7 +6,8 @@ const geoip = require('geoip-lite');
 
 const rootMiddleware = (app) => {
     app.use(cors());
-    app.use(express.json());
+    app.use(express.json({ limit: '2048mb' }));
+    app.use(express.urlencoded({ limit: '2048mb', extended: true }));
 
     //file upload middleware
     app.use(fileUpload(
